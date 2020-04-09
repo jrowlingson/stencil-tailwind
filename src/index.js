648f4e6152233c98f62726ee650e073b2d8a938f
Jack Rowlingson <jack.rowlingson@gmail.com>
2020-04-09 12:24:57 -0400

    fix styles concat

--- src/index.js
+++ src/index.js
@@ -2,27 +2,26 @@ const MagicString = require('magic-string')
 const fs = require('fs')
 const postcss = require('postcss')
 const tailwindcss = require('tailwindcss')
-// const { blue, magenta, red } = require('chalk')
 
 // const log = console.log
+// const { blue, magenta, red } = require('chalk')
 
 let stylesTree
 
 function _staticStyles(code) {
-  if (code.match(/class: `(.+?)`/gs)) {
-    let tokens = code.replace(/[\n'}]+/gm, '').match(/class: `(.+?)`/gs)
-    console.log(tokens)
+  if (code.match(/class: "(.+?)"/gs)) {
+    let tokens = code.replace(/[\n'}]+/gm, '').match(/class: "(.+?)"/gs)
     tokens = tokens
-      .map(str =>  str.match(/`(.*)`/)[1])
+      .map(str =>  str.match(/"(.*)"/)[1])
       .join(' ')
       .split(' ')
       .filter(s => s !== '')
     const result = stylesTree.root.nodes.reduce((acc, node) => {
-        if (node.selector && tokens.includes(node.selector.substring(1))) {
-          return node.toString().replace(/(\r\n|\n|\r)/gm, '') + ' \\n' + acc
-        }
-        return acc
-      }, ' S')
+      if (node.selector && tokens.includes(node.selector.substring(1))) {
+        return node.toString().replace(/(\r\n|\n|\r)/gm, '') + ' \\n' + acc
+      }
+      return acc
+    }, '')
     return result
   }
 }
@@ -47,13 +46,12 @@ function rollupPluginTailwind() {
     transform(code, id) {
       if (id.includes('components')) {
         if (id.includes('checkbox')) {
-          let match = /STYLE_TEXT_PLACEHOLDER/.exec(code)
+          let match = /= (.*?Style);/.exec(code)
           if (match) {
             const s = new MagicString(code)
-            // log(magenta("OVERWRITE:", s.toString()))
-            const styles =  _staticStyles(code)
+            const styles = _staticStyles(code)
             if (styles) {
-              s.overwrite(match.index, match.index + 1, styles)
+              s.overwrite(match.index, match.index + match[1].length + 2,`= \`${styles} \${${match[1]}}\``)
             }
             return {
               code: s.toString()
